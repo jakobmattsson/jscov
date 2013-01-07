bc84cd30e18d91626f9ee4d86aef6c679c43182b
Jakob Mattsson <jakob.mattsson@gmail.com>
2013-01-07 18:27:54 +0100

    test for trailing semicolon in return statement

--- spec/scaffold/hardcoded-method.js
+++ spec/scaffold/hardcoded-method.js
@@ -1,34 +1,4 @@
-x["y"].f();
-x["with"].f();
-x["6"].f();
-x["06"].f();
-x["0xAB"].f();
-x["6x"].f();
-x["+++y"].f();
-
-
-while (true) { }
-while ("hej") { }
-while (123) { }
-
-
-n = 1e+400
-m = Number.POSITIVE_INFINITY
-
-p =  1 /  0
-q = -1 /  0
-r =  1 / -0
-s = -1 / -0
-
-
-
-// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDER “AS IS”
-
-str = "HOLDER “AS IS”"
-
-str = "åäö"
-
-myvaråäö = 1
-
-
-
+function trailing_semicolon() {
+  return 1
+  ;
+}
